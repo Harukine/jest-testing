c67fdd90791afc86ffe7fc6588629a77d62a67ee
Jacob <jacobhtnguyen@gmail.com>
2020-03-08 20:41:53 -0400

    created tests for promises

---
+++ src/asynchronous/promises.spec.js
@@ -0,0 +1,20 @@
+// testing promises
+async function fetchData() {
+    return 'peanut butter';
+}
+async function fetchDataFail() {
+    throw 'error';
+}
+
+// returning a promise
+test('the data is peanut butter', () => {
+    return fetchData().then(data => {
+        expect(data).toBe('peanut butter');
+    });
+});
+
+// testing catch
+test('the fetch fails with an error', () => {
+    expect.assertions(1);
+    return fetchDataFail().catch(e => expect(e).toMatch('error'));
+});
